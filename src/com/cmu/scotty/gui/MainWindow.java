6aa0430c352296ba8ae262c9b735b0e4266618ff
Daniel Alan Miller <dalan.miller@gmail.com>
2013-12-02 18:30:23 +1030

    updating to are more Mac-esque window style

--- src/com/cmu/scotty/gui/MainWindow.java
+++ src/com/cmu/scotty/gui/MainWindow.java
@@ -1,18 +1,21 @@
 package com.cmu.scotty.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
 
 public class MainWindow {
 
@@ -24,7 +27,6 @@ public class MainWindow {
 	private JButton jbtImport;
 	private JButton jbtFilter;
 	private JButton jbtExport;
-	private JMenuBar jmb = new JMenuBar();
 	
 
 	/**
@@ -55,16 +57,19 @@ public class MainWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setFont(new Font("Helvetica", Font.PLAIN, 12));
+		frame.setForeground(Color.WHITE);
+		frame.setBackground(Color.WHITE);
+		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//menu
-		JMenu jmSkin = new JMenu("Skins");
-		jmb.add(jmSkin);
 		
 		
 		//	TEST CODE
 		JLabel lblThisIsPanel = new JLabel("Import File");
+		jpImport.setBorder(null);
+		jpImport.setBackground(Color.WHITE);
+		jpImport.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		jpImport.add(lblThisIsPanel);
 		
 		JLabel lblThisIsPane2 = new JLabel("PanelFilter");
@@ -72,29 +77,16 @@ public class MainWindow {
 		
 		JLabel lblThisIsPane3 = new JLabel("PanelExport");
 		jpExport.add(lblThisIsPane3);
-		
-		
-		
-		//Left Side-Buttons
-		jbtImport = new JButton("Import");
-		jbtImport.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				jpImport.setVisible(true);
-				jpFilter.setVisible(false);
-				jpExport.setVisible(false);
-				frame.getContentPane().add(jpImport, BorderLayout.CENTER);
-			}
-		});
-		jbtFilter = new JButton("Filter");
-		jbtFilter.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				jpImport.setVisible(false);
-				jpFilter.setVisible(true);
-				jpExport.setVisible(false);
-				frame.getContentPane().add(jpFilter, BorderLayout.CENTER);
-			}
-		});
+		jpStatic.setBorder(null);
+		jpStatic.setForeground(Color.WHITE);
+		jpStatic.setBackground(Color.WHITE);
+
+		// MainWindow layout
+		frame.getContentPane().add(jpStatic, BorderLayout.NORTH);
 		jbtExport = new JButton("Export");
+		jbtExport.setBackground(Color.WHITE);
+		jbtExport.setForeground(Color.DARK_GRAY);
+		jbtExport.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
 		jbtExport.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				jpImport.setVisible(false);
@@ -105,24 +97,45 @@ public class MainWindow {
 		});
 		
 		
-		//Panels
-		jpStatic.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		//Left Side-Buttons
+		jbtImport = new JButton("Import");
+		jbtImport.setForeground(Color.DARK_GRAY);
+		jbtImport.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		jbtImport.setBackground(Color.WHITE);
+		jbtImport.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				jpImport.setVisible(true);
+				jpFilter.setVisible(false);
+				jpExport.setVisible(false);
+				frame.getContentPane().add(jpImport, BorderLayout.CENTER);
+			}
+		});
+		jpStatic.setLayout(new GridLayout(1, 3, 0, 0));
 		jpStatic.add(jbtImport);
+		jbtFilter = new JButton("Filter");
+		jbtFilter.setForeground(Color.DARK_GRAY);
+		jbtFilter.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+		jbtFilter.setBackground(Color.WHITE);
+		jbtFilter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				jpImport.setVisible(false);
+				jpFilter.setVisible(true);
+				jpExport.setVisible(false);
+				frame.getContentPane().add(jpFilter, BorderLayout.CENTER);
+			}
+		});
 		jpStatic.add(jbtFilter);
 		jpStatic.add(jbtExport);
-		
-		
-	
-		
-		
-		//MainWindow layout
-		frame.getContentPane().add(jpStatic, BorderLayout.WEST);
 		frame.getContentPane().add(jpFilter, BorderLayout.CENTER);
 		frame.getContentPane().add(jpExport, BorderLayout.CENTER);
 		jpFilter.setVisible(false);
 		jpFilter.setVisible(false);
 		frame.getContentPane().add(jpImport, BorderLayout.CENTER);
-		frame.setJMenuBar(jmb);
+		frame.setFocusTraversalPolicy(new FocusTraversalOnArray(
+				new Component[] { frame.getContentPane(), jpStatic, jbtImport,
+						jbtFilter, jbtExport, jpFilter, lblThisIsPane2,
+						jpExport, lblThisIsPane3, jpImport, lblThisIsPanel }));
 		
 	}
 
