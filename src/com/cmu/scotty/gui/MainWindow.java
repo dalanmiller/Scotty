4de2f2cba919e4bb8c02b60cdc83acc84bf2f1b3
Daniel Alan Miller <dalan.miller@gmail.com>
2013-12-02 18:52:08 +1030

    Mac-esque, again

--- src/com/cmu/scotty/gui/MainWindow.java
+++ src/com/cmu/scotty/gui/MainWindow.java
@@ -15,18 +15,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-<<<<<<< HEAD
-import java.awt.FlowLayout;
-
-import javax.swing.BoxLayout;
-
-import java.awt.CardLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import javax.swing.SwingConstants; //similar to the last one, nice 
-=======
 import org.eclipse.wb.swing.FocusTraversalOnArray;
->>>>>>> 1891ea21debce5b2f989ae6306e04f27364dd1d4
 
 public class MainWindow {
 
@@ -40,39 +29,6 @@ public class MainWindow {
 	private JButton jbtExport;
 	
 
-<<<<<<< HEAD
-//	private FileNameExtensionFilter filterTxt = new FileNameExtensionFilter("txt");
-	
-	//Panel Import
-	private JPanel jpImportExcel = new JPanel();
-	private JPanel jpImportTxt = new JPanel();
-	private JPanel jpImportImg = new JPanel(); 
-	private JPanel jpImportNext = new JPanel();
-	//Panel excel import
-	private JLabel lblImportExcel = new JLabel("Excel file");
-	private JTextField jtfExcelPath = new JTextField();
-	private JButton jbtBrowseExcel = new JButton("Browse");
-	private File importExcelFile = new File(""); 
-	private FileNameExtensionFilter filterExcel = new FileNameExtensionFilter("Excel", "xls", "xlsx");
-	private JFileChooser jfcImportExcelFile = new JFileChooser();
-	//Panel Txt Import
-	private JLabel lblImportTxt = new JLabel("Txt file");
-	private JTextField jtfTxtPath = new JTextField();
-	private JButton jbtBrowseTxt = new JButton("Browse");
-	private File importTxtFile = new File(""); 
-	private FileNameExtensionFilter filterTxt = new FileNameExtensionFilter("TXT", "txt");
-	private JFileChooser jfcImportTxtFile = new JFileChooser();
-	//Panel Folder Import
-	private JLabel lblImportImg = new JLabel("Image folder");
-	private JTextField jtfImgPath = new JTextField();
-	private JButton jbtBrowseImg = new JButton("Browse");
-	private File importImgFile = new File(""); 
-	private JFileChooser jfcImportImgFile = new JFileChooser();
-	//Panel Next Import
-	private JButton jbtImportNext = new JButton("Next>>");
-	
-=======
->>>>>>> 1891ea21debce5b2f989ae6306e04f27364dd1d4
 	/**
 	 * Launch the application.
 	 */
@@ -171,86 +127,6 @@ public class MainWindow {
 		});
 		jpStatic.add(jbtFilter);
 		jpStatic.add(jbtExport);
-<<<<<<< HEAD
-		
-		
-		
-		//PanelImport
-		jpImport.setLayout(new GridLayout(4, 1, 0, 0));
-		jpImport.add(jpImportExcel);
-		jpImport.add(jpImportTxt);
-		jpImport.add(jpImportImg);
-		jpImport.add(jpImportNext);
-		//PanelExcelImport
-		jpImportExcel.setLayout(null);
-		lblImportExcel.setBounds(28, 31, 69, 15);
-		jpImportExcel.add(lblImportExcel);
-		jtfExcelPath.setBounds(107, 27, 167, 23);
-		jpImportExcel.add(jtfExcelPath);
-		jfcImportExcelFile.setFileFilter(filterExcel);
-		jbtBrowseExcel.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				int returnVal = jfcImportExcelFile.showOpenDialog(jbtBrowseExcel);			
-				if(returnVal == JFileChooser.APPROVE_OPTION){
-					importExcelFile = jfcImportExcelFile.getSelectedFile();
-					jtfExcelPath.setText(importExcelFile.getAbsolutePath());
-				}
-			}
-		});
-		jbtBrowseExcel.setBounds(286, 27, 69, 23);
-		jpImportExcel.add(jbtBrowseExcel);
-		
-		//PanelTxtImport
-		jpImportTxt.setLayout(null);
-		lblImportTxt.setBounds(28, 34, 69, 15);
-		jpImportTxt.add(lblImportTxt);
-		jtfTxtPath.setBounds(107, 30, 167, 23);
-		jpImportTxt.add(jtfTxtPath);
-		jfcImportTxtFile.setFileFilter(filterTxt);
-		jbtBrowseTxt.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				int returnVal = jfcImportTxtFile.showOpenDialog(jbtBrowseTxt);			
-				if(returnVal == JFileChooser.APPROVE_OPTION){
-					importTxtFile = jfcImportTxtFile.getSelectedFile();
-					jtfTxtPath.setText(importTxtFile.getAbsolutePath());
-				}
-			}
-		});
-		jbtBrowseTxt.setBounds(286, 30, 69, 23);
-		jpImportTxt.add(jbtBrowseTxt);
-		//PanelImgPath
-		jfcImportImgFile.setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
-		jpImportImg.setLayout(null);
-		lblImportImg.setBounds(28, 34, 69, 15);
-		jpImportImg.add(lblImportImg);
-		jtfImgPath.setBounds(107, 30, 167, 23);
-		jpImportImg.add(jtfImgPath);
-	//	jfcImportTxtFile.setFileFilter(filterImg);
-		jbtBrowseImg.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				int returnVal = jfcImportImgFile.showOpenDialog(jbtBrowseImg);			
-				if(returnVal == JFileChooser.APPROVE_OPTION){
-					importImgFile = jfcImportImgFile.getSelectedFile();
-					jtfImgPath.setText(importImgFile.getAbsolutePath());
-				}
-			}
-		});
-		jbtBrowseImg.setBounds(286, 30, 69, 23);
-		jpImportImg.add(jbtBrowseImg);
-		jpImportNext.setLayout(null);
-		jpImportNext.setLayout(null);
-		jbtImportNext.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		jbtImportNext.setBounds(286, 27, 69, 23);
-		jpImportNext.add(jbtImportNext);
-		
-		//MainWindow layout
-		frame.getContentPane().add(jpStatic, BorderLayout.WEST);
-=======
->>>>>>> 1891ea21debce5b2f989ae6306e04f27364dd1d4
 		frame.getContentPane().add(jpFilter, BorderLayout.CENTER);
 		frame.getContentPane().add(jpExport, BorderLayout.CENTER);
 		jpFilter.setVisible(false);
